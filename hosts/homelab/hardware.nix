d2892a8cafff154e417661668834d6cbed262062
Justin Chen <42143810+StarryReverie@users.noreply.github.com>
2025-10-23 00:32:35 +0800

    feat(hosts/homelab): enable fstrim for SSD

--- hosts/homelab/hardware.nix
+++ hosts/homelab/hardware.nix
@@ -73,6 +73,8 @@
     { device = "/dev/disk/by-uuid/fd164910-91cf-4216-9605-d9f2248bb0cb"; }
   ];
 
+  services.fstrim.enable = true;
+
   services.logind.settings.Login = {
     HandleLidSwitchExternalPower = "ignore";
     HandleLidSwitchDocked = "ignore";
