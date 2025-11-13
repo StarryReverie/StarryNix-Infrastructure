cf58773d0646ffa837afd00d7c1fb586eac421fc
Justin Chen <42143810+StarryReverie@users.noreply.github.com>
2025-11-14 00:34:27 +0800

    refactor(hosts/homelab): drop home-manager

--- hosts/homelab/entry-point.nix
+++ hosts/homelab/entry-point.nix
@@ -3,23 +3,60 @@
   constants,
   flakeRoot,
   ...
-}@args:
+}@specialArgs:
 inputs.nixpkgs.lib.nixosSystem {
   inherit (constants) system;
-
-  specialArgs = args;
+  inherit specialArgs;
 
   modules = [
-    inputs.home-manager.nixosModules.home-manager
+    # External modules
     inputs.nix-maid.nixosModules.default
+
+    # StarryNix-Infrastructure
     (flakeRoot + /modules/nixos/starrynix-infrastructure/host)
+    (flakeRoot + /nodes/registry.nix)
+
+    # NixOS modules
+    (flakeRoot + /modules/nixos/dae)
+    (flakeRoot + /modules/nixos/nix)
+    (flakeRoot + /modules/nixos/openssh)
+    (flakeRoot + /modules/nixos/secret)
+    ./hardware.nix
+    ./networking.nix
+    ./service.nix
     ./system.nix
 
-    {
-      home-manager.useUserPackages = true;
-      home-manager.users.${constants.username} = import ./home.nix;
-      home-manager.extraSpecialArgs = args;
-      home-manager.backupFileExtension = "bak";
-    }
+    # Home modules (wrapper-manager + nix-maid)
+    ({ config, pkgs, ... }: {
+      imports = [
+        (flakeRoot + /modules/home/wrapper/system-options.nix)
+
+        (flakeRoot + /modules/home/nix-maid/direnv)
+      ];
+
+      wrapperConfigurations = {
+        inherit pkgs;
+        modules = [
+          (flakeRoot + /modules/home/wrapper/wrapper-options.nix)
+          { wrapperConfigurations.finalPackages = config.wrapperConfigurations.finalPackages; }
+
+          (flakeRoot + /modules/home/wrapper/atuin)
+          (flakeRoot + /modules/home/wrapper/bat)
+          (flakeRoot + /modules/home/wrapper/difftastic)
+          (flakeRoot + /modules/home/wrapper/direnv)
+          (flakeRoot + /modules/home/wrapper/eza)
+          (flakeRoot + /modules/home/wrapper/fastfetch)
+          (flakeRoot + /modules/home/wrapper/fd)
+          (flakeRoot + /modules/home/wrapper/fzf)
+          (flakeRoot + /modules/home/wrapper/git)
+          (flakeRoot + /modules/home/wrapper/helix)
+          (flakeRoot + /modules/home/wrapper/lazygit)
+          (flakeRoot + /modules/home/wrapper/ripgrep)
+          (flakeRoot + /modules/home/wrapper/zellij)
+          (flakeRoot + /modules/home/wrapper/zoxide)
+          (flakeRoot + /modules/home/wrapper/zsh)
+        ];
+      };
+    })
   ];
 }
--- hosts/homelab/system.nix
+++ hosts/homelab/system.nix
@@ -8,46 +8,9 @@
   ...
 }:
 {
-  imports = [
-    (flakeRoot + /modules/nixos/dae)
-    (flakeRoot + /modules/nixos/nix)
-    (flakeRoot + /modules/nixos/openssh)
-    (flakeRoot + /modules/nixos/secret)
-    (flakeRoot + /nodes/registry.nix)
-    (flakeRoot + /modules/home/nix-maid/direnv)
-    (flakeRoot + /modules/home/wrapper/system-options.nix)
-    ./hardware.nix
-    ./networking.nix
-    ./service.nix
-  ];
-
   networking.hostName = constants.hostname;
   time.timeZone = "Asia/Shanghai";
   i18n.defaultLocale = "en_US.UTF-8";
-
-  wrapperConfigurations = {
-    inherit pkgs;
-    modules = [
-      (flakeRoot + /modules/home/wrapper/wrapper-options.nix)
-      { wrapperConfigurations.finalPackages = config.wrapperConfigurations.finalPackages; }
-
-      (flakeRoot + /modules/home/wrapper/atuin)
-      (flakeRoot + /modules/home/wrapper/bat)
-      (flakeRoot + /modules/home/wrapper/difftastic)
-      (flakeRoot + /modules/home/wrapper/direnv)
-      (flakeRoot + /modules/home/wrapper/eza)
-      (flakeRoot + /modules/home/wrapper/fastfetch)
-      (flakeRoot + /modules/home/wrapper/fd)
-      (flakeRoot + /modules/home/wrapper/fzf)
-      (flakeRoot + /modules/home/wrapper/git)
-      (flakeRoot + /modules/home/wrapper/helix)
-      (flakeRoot + /modules/home/wrapper/lazygit)
-      (flakeRoot + /modules/home/wrapper/ripgrep)
-      (flakeRoot + /modules/home/wrapper/zellij)
-      (flakeRoot + /modules/home/wrapper/zoxide)
-      (flakeRoot + /modules/home/wrapper/zsh)
-    ];
-  };
 
   users.users.${constants.username} = {
     isNormalUser = true;
