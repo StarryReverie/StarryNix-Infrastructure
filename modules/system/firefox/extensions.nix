71bc865debbbbc6420e8ed41eb3020362aa739e4
Justin Chen <42143810+StarryReverie@users.noreply.github.com>
2025-12-17 18:53:48 +0800

    feat(modules/system/firefox): add more extensions

--- modules/system/firefox/extensions.nix
+++ modules/system/firefox/extensions.nix
@@ -16,15 +16,19 @@
       };
     in
     lib.attrsets.listToAttrs [
+      (extension "auto-tab-discard" "{c2c003ee-bd69-42a2-b0e9-6f34222cb046}")
       (extension "canvasblocker" "CanvasBlocker@kkapsner.de")
       (extension "clearurls" "{74145f27-f039-47ce-a470-a662b129930a}")
       (extension "darkreader" "addon@darkreader.org")
+      (extension "decentraleyes" "jid1-BoFifL9Vbdl2zQ@jetpack")
+      (extension "i-dont-care-about-cookies" "jid1-KKzOGWgsW3Ao4Q@jetpack")
       (extension "keepassxc-browser" "keepassxc-browser@keepassxc.org")
       (extension "privacy-badger17" "jid1-MnnxcxisBPnSXQ@jetpack")
       (extension "scroll_anywhere" "juraj.masiar@gmail.com_ScrollAnywhere")
       (extension "skip-redirect" "skipredirect@sblask")
       (extension "smart-https-revived" "{b3e677f4-1150-4387-8629-da738260a48e}")
-      (extension "ublock-origin" "uBlock0@raymondhill.net")
       (extension "tampermonkey" "firefox@tampermonkey.net")
+      (extension "textarea-cache" "textarea-cache-lite@wildsky.cc")
+      (extension "ublock-origin" "uBlock0@raymondhill.net")
     ];
 }
