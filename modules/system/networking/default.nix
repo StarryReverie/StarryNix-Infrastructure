ba915b9ab447cae1650221946a272d6e901db022
Justin Chen <42143810+StarryReverie@users.noreply.github.com>
2025-12-18 21:08:39 +0800

    refactor(modules/system/networking): remove obsoleted configurations

--- modules/system/networking/default.nix
+++ modules/system/networking/default.nix
@@ -13,21 +13,22 @@
     "8.8.8.8"
   ];
 
-  environment.etc."systemd/network/99-wireless-client-dhcp.network.d/40-disable-dhcp-dns.conf".text =
-    ''
-      [DHCPv4]
-      UseDNS=false
+  # Partially sourced from configurations in nixpkgs
+  systemd.network.networks."99-ethernet-default-dhcp" = {
+    # Original configurations, duplicated in case that these settings are not
+    # enabled due to `networking.useDHCP = false`. Disabling DHCP is required
+    # if NetworkManager is enabled. But NetworkManager only manages wireless
+    # interfaces as specified in this repository, thus DHCP settings for
+    # ethernets should be kept.
+    matchConfig = {
+      Type = "ether";
+      Kind = "!*";
+    };
+    DHCP = "yes";
+    networkConfig.IPv6PrivacyExtensions = "kernel";
 
-      [DHCPv6]
-      UseDNS=false
-    '';
-
-  environment.etc."systemd/network/99-ethernet-default-dhcp.network.d/40-disable-dhcp-dns.conf".text =
-    ''
-      [DHCPv4]
-      UseDNS=false
-
-      [DHCPv6]
-      UseDNS=false
-    '';
+    # Additional configurations
+    dhcpV4Config.UseDNS = false;
+    dhcpV6Config.UseDNS = false;
+  };
 }
