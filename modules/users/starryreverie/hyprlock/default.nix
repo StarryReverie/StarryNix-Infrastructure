074ff634dd269cfe62b2634aa908ccdffcf5369c
Justin Chen <42143810+StarryReverie@users.noreply.github.com>
2026-01-08 19:42:15 +0800

    feat(modules/user/starryreverie/hyprlock): change background

--- modules/users/starryreverie/hyprlock/default.nix
+++ modules/users/starryreverie/hyprlock/default.nix
@@ -2,13 +2,26 @@
   config,
   lib,
   pkgs,
+  inputs,
   ...
 }:
+let
+  resourcesPkgs = inputs.starrynix-resources.legacyPackages.${pkgs.stdenv.hostPlatform.system};
+  wallpaperPackage = resourcesPkgs.wallpaperPackages.minimalism;
+in
 {
   users.users.starryreverie.maid = {
     packages = with pkgs; [ hyprlock ];
 
-    file.xdg_config."hypr/hyprlock.conf".source = ./hyprlock.conf;
+    file.xdg_config."hypr/hyprlock.conf".text = ''
+      ${builtins.readFile ./hyprlock.template.conf}
+
+      background {
+          path = ${wallpaperPackage.wallpaperDir}/wallhaven-2y2wg6.jpg
+          blur_passes = 2
+          blur_size = 5
+      }
+    '';
   };
 
   security.pam.services.hyprlock = { };
