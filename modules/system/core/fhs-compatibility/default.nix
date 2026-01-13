2599e276b386c9f0093225266caa5b447b1cab6f
Justin Chen <42143810+StarryReverie@users.noreply.github.com>
2026-01-13 20:31:45 +0800

    feat(m/s/c/fhs-compatibility): add envfs

--- modules/system/core/fhs-compatibility/default.nix
+++ modules/system/core/fhs-compatibility/default.nix
@@ -6,4 +6,27 @@
 }:
 {
   programs.nix-ld.enable = true;
+
+  services.envfs.enable = true;
+
+  # See <https://github.com/Mic92/envfs/issues/203>.
+  fileSystems."/bin".enable = false;
+  fileSystems."/usr/bin".options = [
+    "x-systemd.requires=modprobe@fuse.service"
+    "x-systemd.after=modprobe@fuse.service"
+  ];
+
+  boot.initrd.systemd.tmpfiles.settings = {
+    "50-usr-bin" = {
+      "/sysroot/usr/bin".d = {
+        user = "root";
+        group = "root";
+        mode = "0755";
+      };
+    };
+  };
+
+  environment.variables = {
+    ENVFS_RESOLVE_ALWAYS = "1";
+  };
 }
