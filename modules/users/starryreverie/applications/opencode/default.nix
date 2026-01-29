1771d232b6aeda98201a27c0a9d09ee950fd8bad
Justin Chen <42143810+StarryReverie@users.noreply.github.com>
2026-01-29 11:47:38 +0800

    feat(m/u/s/a/opencode): store LLM API keys as secret

--- modules/users/starryreverie/applications/opencode/default.nix
+++ modules/users/starryreverie/applications/opencode/default.nix
@@ -10,10 +10,16 @@ let
 in
 {
   config = lib.mkIf customCfg.enable {
-    users.users.starryreverie = {
-      maid = {
-        packages = with pkgs; [ opencode ];
-      };
+    users.users.starryreverie.maid = {
+      packages = with pkgs; [ opencode ];
+
+      file.xdg_data."opencode/auth.json".source =
+        config.age.secrets."starryreverie-opencode-auth.json".path;
+    };
+
+    age.secrets."starryreverie-opencode-auth.json" = {
+      rekeyFile = ./starryreverie-opencode-auth.json.age;
+      configureForUser = config.users.users.starryreverie.name;
     };
 
     preservation.preserveAt."/nix/persistence" = {
