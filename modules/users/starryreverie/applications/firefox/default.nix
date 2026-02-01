f01238ad7ce2e924718087ce5336a32bc58261be
Justin Chen <42143810+StarryReverie@users.noreply.github.com>
2026-02-01 19:50:06 +0800

    feat(m/u/s/a/firefox): migrate configuration to XDG user directory

--- modules/users/starryreverie/applications/firefox/default.nix
+++ modules/users/starryreverie/applications/firefox/default.nix
@@ -5,12 +5,30 @@
   flakeRoot,
   ...
 }:
+let
+  selfCfg = config.custom.users.starryreverie;
+  customCfg = selfCfg.applications.firefox;
+in
 {
-  config = lib.mkIf config.programs.firefox.enable {
+  config = lib.mkIf customCfg.enable {
     preservation.preserveAt."/nix/persistence" = {
       users.starryreverie = {
-        directories = [ ".mozilla" ];
+        directories = [ ".config/mozilla/firefox" ];
       };
+    };
+
+    users.users.starryreverie.maid = {
+      file.xdg_config."mozilla/firefox/native-messaging-hosts/org.keepassxc.keepassxc_browser.json".source =
+        lib.mkIf selfCfg.applications.keepassxc.enable (
+          pkgs.replaceVars ./native-messaging-hosts-keepassxc.json {
+            keepassxcProxyPath = "${pkgs.keepassxc}/bin/keepassxc-proxy";
+          }
+        );
+
+      # FIXME: Firefox's migration to XDG User Directory Specification is not completed. This
+      # directory can't be moved currently.
+      file.home.".mozilla/native-messaging-hosts".source =
+        "{{xdg_config_home}}/mozilla/firefox/native-messaging-hosts";
     };
   };
 }
