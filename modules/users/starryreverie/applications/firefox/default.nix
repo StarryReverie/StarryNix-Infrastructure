376486cfee305050d308ee330845d531dbaf1b52
Justin Chen <42143810+StarryReverie@users.noreply.github.com>
2026-02-21 18:38:01 +0800

    fix(m/u/s/a/firefox): eval failure when KeePassXC is not enabled

--- modules/users/starryreverie/applications/firefox/default.nix
+++ modules/users/starryreverie/applications/firefox/default.nix
@@ -10,25 +10,29 @@ let
   customCfg = selfCfg.applications.firefox;
 in
 {
-  config = lib.mkIf customCfg.enable {
-    preservation.preserveAt."/nix/persistence" = {
-      users.starryreverie = {
-        directories = [ ".config/mozilla/firefox" ];
+  config = lib.mkMerge [
+    (lib.mkIf customCfg.enable {
+      preservation.preserveAt."/nix/persistence" = {
+        users.starryreverie = {
+          directories = [ ".config/mozilla/firefox" ];
+        };
       };
-    };
 
-    users.users.starryreverie.maid = {
-      file.xdg_config."mozilla/firefox/native-messaging-hosts/org.keepassxc.keepassxc_browser.json".source =
-        lib.mkIf selfCfg.applications.keepassxc.enable (
+      users.users.starryreverie.maid = {
+        # FIXME: Firefox's migration to XDG User Directory Specification is not completed. This
+        # directory can't be moved currently.
+        file.home.".mozilla/native-messaging-hosts".source =
+          "{{xdg_config_home}}/mozilla/firefox/native-messaging-hosts";
+      };
+    })
+
+    (lib.mkIf (customCfg.enable && selfCfg.applications.keepassxc.enable) {
+      users.users.starryreverie.maid = {
+        file.xdg_config."mozilla/firefox/native-messaging-hosts/org.keepassxc.keepassxc_browser.json".source =
           pkgs.replaceVars ./native-messaging-hosts-keepassxc.json {
             keepassxcProxyPath = "${pkgs.keepassxc}/bin/keepassxc-proxy";
-          }
-        );
-
-      # FIXME: Firefox's migration to XDG User Directory Specification is not completed. This
-      # directory can't be moved currently.
-      file.home.".mozilla/native-messaging-hosts".source =
-        "{{xdg_config_home}}/mozilla/firefox/native-messaging-hosts";
-    };
-  };
+          };
+      };
+    })
+  ];
 }
