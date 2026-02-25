c1cf5a66ba2eb822bb5b9958acf737e648e84398
Justin Chen <42143810+StarryReverie@users.noreply.github.com>
2026-02-25 23:00:45 +0800

    feat(m/s/c/nix): add cache.garnix.io as substituter

--- modules/system/core/nix/default.nix
+++ modules/system/core/nix/default.nix
@@ -23,6 +23,7 @@ in
       "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
       "https://cache.nixos.org"
 
+      "https://cache.garnix.io"
       "https://nix-community.cachix.org"
       "https://colmena.cachix.org"
     ];
@@ -30,6 +31,7 @@ in
     nix.settings.trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
 
+      "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "colmena.cachix.org-1:7BzpDnjjH8ki2CT3f6GdOk7QAzPOl+1t3LvTLXqYcSg="
     ];
