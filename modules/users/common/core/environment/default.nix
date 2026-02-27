4a8010f513cad45343d9f603f2ec4bcfcb69d37f
Justin Chen <42143810+StarryReverie@users.noreply.github.com>
2026-02-27 22:12:01 +0800

    feat(m/u/c/c/environment): allow setting variables for graphical sessions

--- modules/users/common/core/environment/default.nix
+++ modules/users/common/core/environment/default.nix
@@ -16,13 +16,24 @@ let
         sessionVariables = lib.mkOption {
           type = lib.types.attrsOf lib.types.str;
           description = ''
-            All session environment variables to be set per-user. Values are
-            directly written to the file in `$XDG_CONFIG_HOME/environment.d/`
-            without escaping.
+            All session environment variables to be set per-user. Values are directly written to the
+            file in `$XDG_CONFIG_HOME/environment.d/` without escaping.
           '';
           default = { };
           example = {
             EDITOR = "hx";
+          };
+        };
+
+        graphicalSessionVariables = lib.mkOption {
+          type = lib.types.attrsOf lib.types.str;
+          description = ''
+            All graphical session environment variables to be set per-user. Values are directly
+            written to the file in `$XDG_CONFIG_HOME/environment.d/` without escaping.
+          '';
+          default = { };
+          example = {
+            LANG = "zh_CN.UTF-8";
           };
         };
       };
@@ -37,12 +48,35 @@ let
     {
       config = lib.mkIf (customCfg.enable or false) {
         maid = {
-          file.home.".profile".text = ''
-            source <(${pkgs.systemd}/lib/systemd/user-environment-generators/30-systemd-environment-d-generator)
-          '';
+          file.home.".profile".text =
+            if customCfg.graphicalSessionVariables == { } then
+              ''
+                source <(${pkgs.systemd}/lib/systemd/user-environment-generators/30-systemd-environment-d-generator)
+              ''
+            else
+              ''
+                if [[ "''${XDG_SESSION_TYPE:-tty}" == "tty" ]]; then
+                  source <(${pkgs.systemd}/lib/systemd/user-environment-generators/30-systemd-environment-d-generator | grep -v ${
+                    lib.pipe customCfg.graphicalSessionVariables [
+                      lib.attrsets.attrNames
+                      (lib.lists.map (name: "-e '^${name}='"))
+                      (lib.strings.concatStringsSep " ")
+                    ]
+                  })
+                else
+                  source <(${pkgs.systemd}/lib/systemd/user-environment-generators/30-systemd-environment-d-generator)
+                fi
+              '';
 
           file.xdg_config."environment.d/60-custom-session-vars.conf".text =
             lib.pipe customCfg.sessionVariables
+              [
+                (lib.attrsets.mapAttrsToList (k: v: "${k}=${v}"))
+                (lib.strings.concatStringsSep "\n")
+              ];
+
+          file.xdg_config."environment.d/60-custom-graphical-session-vars.conf".text =
+            lib.pipe customCfg.graphicalSessionVariables
               [
                 (lib.attrsets.mapAttrsToList (k: v: "${k}=${v}"))
                 (lib.strings.concatStringsSep "\n")
