73bfdc3b857337a2913a18fedcd84b19ab914f3d
Justin Chen <42143810+StarryReverie@users.noreply.github.com>
2026-03-04 08:36:37 +0800

    feat(m/u/c/d/swww): drop blurred background display

--- modules/users/common/desktop/swww/default.nix
+++ modules/users/common/desktop/swww/default.nix
@@ -33,10 +33,7 @@ let
           type = lib.types.package;
           description = ''
             The manager that spawns `swww-daemon` and periodically changing the wallpaper in the
-            background. It can be directly placed in systemd service's `ExecStart`. Two daemon will
-            be spawned, one for blurred background (in namespace swww-daemon@blurred) and another
-            one for the wallpaper without the blurry effect (in namespace `swww-daemon@original`)
-            (readonly)
+            background. (readonly)
           '';
           readOnly = true;
         };
@@ -45,25 +42,16 @@ let
       config = lib.mkIf customCfg.enable {
         desktop.swww = {
           managerPackage = pkgs.writeShellScriptBin "swww-manager" ''
-            ${pkgs.swww}/bin/swww-daemon --namespace "@original" --no-cache &
-            ${pkgs.swww}/bin/swww-daemon --namespace "@blurred" --no-cache &
-
-            tmp_dir="$(mktemp -d ''${TMPDIR:-/tmp}/swww-manager.XXXXXX)"
-
-            trap "rm -rf \"$tmp_dir\"; pkill swww-daemon" EXIT
+            ${pkgs.swww}/bin/swww-daemon --no-cache &
+            trap "pkill swww-daemon" EXIT
 
             function show_image() {
               original_image="$1"
               # Showing wallpaper may fail right after the startup of the daemon
               while true; do
-                ${pkgs.swww}/bin/swww img --namespace "@original" "$original_image" && break
+                ${pkgs.swww}/bin/swww img "$original_image" && break
                 sleep 0.25
               done
-
-              blurred_image="$tmp_dir/blurred-$(basename $original_image)"
-              ${pkgs.imagemagick}/bin/magick "$original_image" -blur 0x7 "$blurred_image"
-              ${pkgs.swww}/bin/swww img --namespace "@blurred" "$blurred_image"
-              rm "$blurred_image"
             }
 
             if [ -f "${customCfg.wallpaperPath}" ]; then
