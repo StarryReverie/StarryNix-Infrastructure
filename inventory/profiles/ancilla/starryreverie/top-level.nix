a6abf0070339c7e961201a1b29fec3e22b2ef7ae
Justin Chen <42143810+StarryReverie@users.noreply.github.com>
2026-08-21 09:01:51 +0800

    profiles/ancilla: wrap helix with configurations

--- inventory/profiles/ancilla/starryreverie/top-level.nix
+++ inventory/profiles/ancilla/starryreverie/top-level.nix
@@ -1,9 +1,39 @@
-{ inputs, pkgs, ... }:
+{
+  inputs,
+  pkgs,
+  flakeRoot,
+  ...
+}:
+let
+  helix-wrapped = inputs.wrapper-manager.lib.wrapWith pkgs {
+    basePackage = pkgs.helix-unwrapped;
+
+    prependFlags = [
+      "--config"
+      "${flakeRoot + /modules/users/starryreverie/applications/helix/config.toml}"
+    ];
+
+    env.HELIX_RUNTIME = builtins.toString (
+      pkgs.symlinkJoin {
+        name = "helix-runtime-extra";
+
+        paths = [
+          pkgs.helix.runtime
+          (pkgs.runCommand "helix-themes-extra" { } ''
+            mkdir -p $out
+            cp -r ${flakeRoot + /modules/users/starryreverie/applications/helix/themes} $out/themes
+          '')
+        ];
+
+      }
+    );
+  };
+in
 {
   paths = with pkgs; [
     difftastic
     direnv
-    helix
+    helix-wrapped
     htop
     lazygit
     zellij
