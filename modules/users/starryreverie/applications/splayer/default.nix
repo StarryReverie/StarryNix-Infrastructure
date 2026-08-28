616726b5bb502c9b0158cce8d7f3a0a7fa3101cb
Justin Chen <42143810+StarryReverie@users.noreply.github.com>
2026-08-28 15:58:58 +0800

    chore: update flake inputs

--- modules/users/starryreverie/applications/splayer/default.nix
+++ modules/users/starryreverie/applications/splayer/default.nix
@@ -12,7 +12,7 @@ in
 {
   config = lib.mkIf (customCfg.enable or false) {
     users.users.starryreverie.maid = {
-      packages = with pkgs; [ splayer ];
+      packages = with pkgs; [ splayer-next ];
     };
 
     preservation.preserveAt."/nix/persistence" = {
