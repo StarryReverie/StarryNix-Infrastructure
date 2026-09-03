a5a073db8435058c5513df0fd5153a95ef96fb0c
Justin Chen <42143810+StarryReverie@users.noreply.github.com>
2026-09-03 15:08:10 +0800

    inventory/hosts: fix unexpected change of NVIDIA DRM device path

--- inventory/hosts/superposition/system/hardware.nix
+++ inventory/hosts/superposition/system/hardware.nix
@@ -58,8 +58,8 @@
       };
 
       hardware.nvidia.prime = {
-        intelBusId = "PCI:0:2:0";
-        nvidiaBusId = "PCI:1:0:0";
+        intelBusId = "PCI:0@0:2:0";
+        nvidiaBusId = "PCI:1@0:0:0";
       };
     }
   ];
--- inventory/hosts/superposition/users/starryreverie/desktop.nix
+++ inventory/hosts/superposition/users/starryreverie/desktop.nix
@@ -11,18 +11,18 @@
       users.users.starryreverie.maid = {
         file.xdg_config."niri/config.kdl".text = lib.mkBefore ''
           debug {
-              render-drm-device "/dev/dri/renderD129"
+              render-drm-device "/dev/dri/by-path/pci-0000:01:00.0-render"
           }
         '';
 
         file.xdg_config."kanshi/config".text = ''
           profile {
-            output "eDP-1" mode 2560x1440@240.000 scale 2
+              output "eDP-1" mode 2560x1440@240.000 scale 2
           }
 
           profile {
-            output "eDP-1" disable
-            output "HDMI-A-1" mode 2560x1440@144.001 scale 2
+              output "eDP-1" disable
+              output "HDMI-A-1" mode 2560x1440@144.001 scale 2
           }
         '';
       };
