b85fc2d23acceb3f0027741d2ce0256fe9e186f7
Justin Chen <42143810+StarryReverie@users.noreply.github.com>
2026-09-05 15:55:53 +0800

    nixTools: don't override nil to use nix of non-default version
    Nix is only in `nativeBuildInputs` of nil, so there is no need to
    override, where rebuilding nil can be avoided

--- modules/users/starryreverie/programs/nixTools/default.nix
+++ modules/users/starryreverie/programs/nixTools/default.nix
@@ -12,9 +12,9 @@ in
   config = lib.mkIf (customCfg.enable or false) {
     users.users.starryreverie.maid = {
       packages = [
-        (pkgs.nil.override { nix = config.nix.package; })
         (pkgs.nixpkgs-review.override { nix = config.nix.package; })
         pkgs.dix
+        pkgs.nil
         pkgs.nix-diff
         pkgs.nix-output-monitor
         pkgs.nix-tree
